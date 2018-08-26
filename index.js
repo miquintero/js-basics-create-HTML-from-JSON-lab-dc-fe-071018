963f4a57b8892a1b65e59bc3eba9143fb0aa1bfa
Alexa Schaeffer <alps231@gmail.com>
2018-08-26 15:30:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-document.addEventListener("DOMContentloaded", (e) => {
-  // invoking a function here will make sure it happens on page load
-  // your code here
-});
+
+function handleJSON(movie) {
+
+  let json = movies[movie]
+  document.getElementById("title").innerHTML = json.title
+  document.getElementById("director").innerHTML = json.director
+  document.getElementById("genre").innerHTML = json.genre
+  document.getElementById("filmRating").innerHTML = json.filmRating
+  document.getElementById("poster").innerHTML = json.poster
+  document.getElementById("description").innerHTML = json.description
+  document.getElementById("cast").innerHTML = ""
+  document.getElementById("review").innerHTML = ""
+  json.cast.forEach(star => {
+    let div = document.createElement('div')
+    div.innerHTML = `<span>${star.actor}</span><span>${star.role}</span>`
+    document.getElementById("cast").appendChild(div)
+  })
+  json.reviews.forEach(review => {
+    let div = document.createElement('div')
+    div.innerHTML = `<span>${review.username}</span><span>${review.content}</span>`
+    document.getElementById("review").appendChild(div)
+  })
+}
+
+  function handleButton(event) {
+    handleJSON (event.target.id)
+  }
+
+  window.onload = () => {
+    handleJSON("Titanic")
+    let buttons = document.getElementByTagName("button")
+    for (let i=0; i < buttons.length; i++) {
+      buttons[i],addEventListener('click', handleButton)
+    }
+  }
